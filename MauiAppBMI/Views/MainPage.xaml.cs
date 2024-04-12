80b1538d6fedc81c85b01f942b3e9ed2707ce62e
Kathy Bauer <kbauer17@my.wctc.edu>
2024-04-12 09:53:40 -0500

    initial commit

---
+++ MauiAppBMI/Views/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace MauiAppBMI.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            var version = typeof(MauiApp).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion;
+            VersionLabel.Text = $".NET MAUI ver. {version?[..version.IndexOf('+')]}";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double weight = Convert.ToDouble(weight_inp.Text);
+            double height = Convert.ToDouble(height_inp.Text);
+            double bmi = (weight / (height * height)) * 703;
+            BMI.Text = $"Your BMI is {bmi}";
+        }
+        
+       
+    }
+}
